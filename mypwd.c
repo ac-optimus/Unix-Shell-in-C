3bc220346031b9d38011277be73dab5c57a76f73
ac-optimus <abhavya.chandra@iitgn.ac.in>
2020-08-25 16:15:22 +0530

    adding pwd

--- mypwd.c
+++ mypwd.c
@@ -1,20 +1,49 @@
 /*
-way : 1
-    open the directory
-    read the directory
-    print the name
-    close the directory
-
+pwd implementation
+    prints name of current/ working directory
+usage
+    mypwd
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+#include <dirent.h>
+
+#define MAX_SIZE 1024
+
+void mypwd(char* dirname){  //is  this void return type good to go?
+    DIR* dir = opendir(dirname); // open the directory location to read.
+
+    if (dir == NULL){
+        // Handelling error
+        fprintf(stderr, "opendir: '%s': %s\n", dirname, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    struct dirent* dir_reader; // to read the directory
+
+    if ((dir_reader = readdir(dir)) !=NULL){
+        printf("%s  ", dir_reader->d_name); // replace tab by \n to print files in new lines
+    }
+    closedir(dir);
+}
 
 
 
-
-
-
-    while ((dir_reader = readdir(dir)) !=NULL){ // see the content in the directory.
-        // pointer shifts to the new file/ directory.
-            stpcpy(cur_file, src);
-            strcat(cur_file, "/");
-            strncat(cur_file, dir_reader->d_name, strlen(dir_reader->d_name));
+int main(int argc, char* argv[]){
+    char dirname[MAX_SIZE] = ""; // initiailzation is so important, else you get garbage values appended at the end.
+    // no argument
+    if (argc == 1){
+        if (getcwd(dirname, sizeof(dirname)) == NULL){
+            fprintf(stderr, "getcwd: '%s': %s\n", dirname, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        else{
+            printf("%s\n",dirname);
+        }
+    }
+    return 0;
+}
