2268458c2d0a329e83ea84105b3cead5f101c5d0
ac-optimus <abhavya.chandra@iitgn.ac.in>
2020-09-01 16:39:00 +0530

    init for grep implementatios

---
+++ mygrep.c
@@ -0,0 +1,93 @@
+/*
+search term is always passed with zero or more files to grep
+should work well for even long lines
+with no command line argument throw an error
+in one approach we can tokenize each line an
+
+*/
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h> //exit()
+#include <string.h>
+
+#define MAX_PATTERN_LENGTH 1024
+
+
+int isPresent(char* line, char* pattern){
+    /* return 1 if a substring else return 0*/
+    int line_length = strlen(line);  //length os line
+    int pattern_length = strlen(pattern); // length of the pattern
+    int k;
+    for (int i = 0; i< line_length; i++){
+        k = 0;
+        // check if pattern is a substring starting with ith index of line
+        while ((i+k < line_length) && (k < pattern_length)){
+            if (line[i + k] != pattern[k]){
+                break;
+            }
+            k++;
+        }
+        if (k == pattern_length){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+void grep(char* filename, char* pattern){
+    FILE* fp = fopen(filename, "r");
+    char* line = NULL;
+    size_t len = 0; // what is this type all about
+    ssize_t nread; // what is this, nread ?  its datatype?
+
+    if (fp == NULL) {
+        // handelling error
+        // printf("wgrep: cannot open file %s\n"filename);
+        fprintf(stderr, "wgrep: '%s': %s\n", filename, strerror(errno));
+        exit(1);
+    }
+
+    int status;
+    while ((nread = getline(&line, &len, fp)) != -1) {
+        status = isPresent(line, pattern);
+        if (status == 1){
+            // print the line
+            printf("%s", line);
+        }
+    }
+    free(line);
+    fclose(fp);
+    // if (status == EOF){
+    //     // handelling error
+    //     fprintf(stderr, "cat: '%s': %s\n", filename, strerror(errno));
+    //     exit(1);
+    // }
+
+}
+
+
+int main(int argc, char* argv[]){
+    char pattern[MAX_PATTERN_LENGTH];
+    if (argc == 1){
+        // what to do here
+        // print the error -
+        // "wgrep: searchterm [file ...]"
+        exit(1);
+    }
+    if (argc == 2){
+        // no filename passed
+        strcpy(pattern, argv[1]);
+        // read the line from input and then call isPresent
+        // pattern = argv[1];
+        // read from the std input and then return the line if it matches
+    }
+    else{
+        strcpy(pattern, argv[1]);
+        for (int i =2; i<argc; i++){
+            grep(argv[i], pattern);
+        }
+    }
+    return 0;
+}
