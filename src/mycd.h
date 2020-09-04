04a99731f131d233a1c552a37167d08071f5f3cb
ac-optimus <abhavya.chandra@iitgn.ac.in>
2020-09-04 18:18:39 +0530

    final cd implementation

--- src/mycd.h
+++ src/mycd.h
@@ -1,18 +1,21 @@
+/*
+cd implementation, used in shell.c
 
-// #include <stdio.h>
-// #include <unistd.h> // chdir
+NOTE:
+    changes directory to default, i.e HOME(project) directory when no argument is passed
+*/
 
 void cd(char** args){
     if (args[2] !=NULL){
         fprintf(stderr, "cd: too many arguments\n");
         return;
     }
-
     char* path = args[1];
-
+    // set path to HOME directory
     if (path == NULL){
         path= HOME; // project home directory
     }
+    // change directory to path
     int status = chdir(path);
     if (status == -1){
         fprintf(stderr, "cd: %s: No such file or directory\n", path);
