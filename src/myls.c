3455d5bdd19ac21e27dbbe6d815002fea27d856c
ac-optimus <abhavya.chandra@iitgn.ac.in>
2020-09-04 17:30:06 +0530

    final ls implementation

--- src/myls.c
+++ src/myls.c
@@ -1,11 +1,14 @@
 /*
 ls implementation
-    - no arguments -- uses the current working directory
-    - one or more arguments -- directories
+
 usage
     myls
     myls dir1 dir2 dir3
+
+NOTE:
+    uses current directory when no argument is passed
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,31 +17,36 @@ usage
 #include <errno.h>
 #include <dirent.h>
 
-#define MAX_SIZE 1024
+#define MAX_FILE_NAME_LENGTH 64
 
-void ls(char* dirname){  //is  this void return type good to go?
-    DIR* dir = opendir(dirname); // open the directory location to read.
 
+void ls(char* dirname){
+    // open the directory location to read
+    DIR* dir = opendir(dirname);
     if (dir == NULL){
         // Handelling error
-        fprintf(stderr, "opendir: '%s': %s\n", dirname, strerror(errno));
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "ls: cannot access '%s': %s\n", dirname, strerror(errno));
+        return;
     }
-    struct dirent* dir_reader; // to read the directory
+    struct dirent* dir_iterator; // to iterate over directory content
 
-    // do not know who the below line is working?
-    while ((dir_reader = readdir(dir)) !=NULL){
-        if ((!strcmp(dir_reader->d_name, ".")) || (!strcmp(dir_reader->d_name, ".."))){
-            // ignore "." and ".." directories;
+    while ((dir_iterator = readdir(dir)) !=NULL){
+        if ((strcmp(dir_iterator->d_name, ".") !=0) && (strcmp(dir_iterator->d_name, "..")!=0)){
+            // ignore "." and ".." directories
+            printf("%s  ", dir_iterator->d_name);
         }
-        else
-            printf("%s\n", dir_reader->d_name); // replace tab by \n to print files in new lines
     }
-    closedir(dir);
+
+    if (closedir(dir) == -1){// close directory
+        // Handelling error
+        fprintf(stderr, "ls: '%s': %s\n", dirname, strerror(errno));
+        return;
+    }
 }
 
+
 int main(int argc, char* argv[]){
-    char dirname[MAX_SIZE] = ""; // initiailzation is so important, else you get garbage values appended at the end.
+    char dirname[MAX_FILE_NAME_LENGTH] = "";
     // no argument
     if (argc == 1){
         if (getcwd(dirname, sizeof(dirname)) == NULL){
@@ -53,8 +61,8 @@ int main(int argc, char* argv[]){
         for (int i=1; i<argc; i++){
             strncpy(dirname, argv[i], strlen(argv[i]));
             if (argc!=2)
-                printf("%s:\n", dirname);
-            ls(dirname);
+                printf("%s:\n", argv[i]);
+            ls(argv[i]);
             printf("\n");
         }
     }
