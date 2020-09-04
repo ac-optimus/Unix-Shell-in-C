fbf420e7cd7bc3f49aae55e0512e537aefe7fa8b
ac-optimus <abhavya.chandra@iitgn.ac.in>
2020-09-04 18:12:14 +0530

    mkdir final implementation

--- src/mymkdir.c
+++ src/mymkdir.c
@@ -1,47 +1,18 @@
 /*
 mkdir implementation
-support multiple direction creation
-usage:
-    mymkdir path_to_dir
-    mymkdir dir1 dir2 dir3 ...
 
+usage:
+    mkdir path_to_dir
+    mkdir dir1 dir2 dir3 ...
 */
 
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-// #include <sys/stat.h>
 #include <stdlib.h>
 #include <errno.h>
 #include "mymkdir.h"
-
-
-#define MAX_SIZE 1024
-
-// void mymkdir(char* pathname){  //void return is not good ??
-
-//     struct stat check_file; // some other name please
-//     // printf("%d\n", stat(pathname, &check_file)); // -1 if does not exist and 0 if exist
-//     if(stat(pathname, &check_file)==0) {
-//         printf("cannot create directory '%s' : File exists\n",pathname);
-//         exit(EXIT_FAILURE);
-//     }
-//     else{
-
-//     int check = mkdir(pathname,0777); // what do do with this number ?
-//     // add a check if a directory with the same file name already exist
-//     // if not then call mkdir, what about the mode?
-//     if (check==-1){
-//         //can not create the directory
-//         fprintf(stderr, "mkdir: '%s': %s\n", pathname, strerror(errno)); // what is the point of adding this line?
-//         exit(EXIT_FAILURE);
-//     }
-//     else{
-//         printf("directory created successfully\n");
-//     }
-//     }
-// }
 
 
 int main(int argc, char* argv[]){
@@ -51,18 +22,11 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }
     else{
-        // we can have a list of args
-
-        char pathname[MAX_SIZE] = "";
+        // handling multiple directory creation
         for (int i=1; i<argc; i++){
-            printf("the directory -- %s\n", argv[i]);
-            strncpy(pathname, argv[i], strlen(argv[i])); //do we need to check for bound MAX_SIZE
             // call mkdir
-            mymkdir(pathname);
-            // printf("%s\n", pathname);
-
+            mymkdir(argv[i]);
         }
     }
-
     return 0;
 }
--- src/mymkdir.h
+++ src/mymkdir.h
@@ -1,35 +1,25 @@
+/* utility function to create new directory */
 #include "isDir.h"
-
-// take care of mkdir p/
-
 
 void mkdir_utility(char* pathname){
 
-    int check = mkdir(pathname,0777); // what do do with this number ?
-        // add a check if a directory with the same file name already exist
-        // if not then call mkdir, what about the mode?
+    int check = mkdir(pathname,0755);
         if (check==-1){
             //can not create the directory
-            fprintf(stderr, "mkdir: '%s': %s\n", pathname, strerror(errno)); // what is the point of adding this line?
-            exit(EXIT_FAILURE);
+            fprintf(stderr, "mkdir: '%s': %s\n", pathname, strerror(errno));
+            return;
         }
 }
 
-void mymkdir(char* path){  //void return is not good ??
+void mymkdir(char* path){
     int status = isDir(path);
+    // check if path already exist
     if ((status == 1) || (status == 2)){
         printf("mkdir: cannot create directory '%s' : File exists\n",path);
-        // exit(EXIT_FAILURE);
-        return; // just do nothing if file already exist
+        return;
     }
-
-    // struct stat check_file; // some other name please
-    // // printf("%d\n", stat(pathname, &check_file)); // -1 if does not exist and 0 if exist
-    // if(stat(pathname, &check_file)==0) {
-    //     printf("mkdir: cannot create directory '%s' : File exists\n",pathname);
-    //     exit(EXIT_FAILURE);
-    // }
     else{
+        //make directory
         mkdir_utility(path);
     }
 }
